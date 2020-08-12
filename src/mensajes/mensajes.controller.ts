eb5b0a1b85b2e2115808047229deec22f8ca902d
jose <jose.zamora@tomzagroup.com>
2020-08-11 22:29:08 -0600

    se crea la clase create-mensaje-dto

--- src/mensajes/mensajes.controller.ts
+++ src/mensajes/mensajes.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Put, Delete } from '@nestjs/common';
+import { CreateMensajeDto } from './dto/create-mensaje-dto';
 
 @Controller('mensajes')
-export class MensajesController {}
+export class MensajesController {
+    @Post()
+    create (@Body() createMensajeDto: CreateMensajeDto) {
+        return 'mensaje creado';
+    }
+
+    @Get()
+    getAll() {
+        return 'lista de mensajes';
+    }
+
+    @Put(':id')
+    update(@Body() updateMensajeDto: CreateMensajeDto) {
+        return 'mensaje actualizado';
+    }
+
+    @Delete(':id')
+    delete() {
+        return 'mensaje eliminado';
+    }
+}
