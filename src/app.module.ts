e8ad5f9b4d297c45b7b6f01fc67f392b26ba41f4
jose <jose.zamora@tomzagroup.com>
2020-08-11 22:38:52 -0600

    se crea el conecto a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MensajesController } from './mensajes/mensajes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'jose',
+      password: 'saque90*.',
+      database: 'sendmeapp_db ',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, MensajesController],
   providers: [AppService],
 })
